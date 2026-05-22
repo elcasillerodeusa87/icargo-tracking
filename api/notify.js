5ab68682df6ca105df6dac76d0a7792b2bc2e366
elcasillerodeusa87 <elcasillerodeusa@gmail.com>
2026-05-22 12:18:34 -0500

    Update notify.js

--- api/notify.js
+++ api/notify.js
@@ -2,8 +2,8 @@ const { Redis } = require('@upstash/redis');
 const fetch = require('node-fetch');
 
 const redis = new Redis({
-  url: process.env.UPSTASH_REDIS_REST_URL,
-  token: process.env.UPSTASH_REDIS_REST_TOKEN,
+url: process.env.KV_REST_API_URL,
+token: process.env.KV_REST_API_TOKEN,
 });
 
 const KEY = 'icargo:guias';
